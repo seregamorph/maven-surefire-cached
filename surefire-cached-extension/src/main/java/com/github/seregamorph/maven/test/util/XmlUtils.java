7ca8afc3617d0b24806ba7fb57612d49db9f0c0f
seregamorph <serega.morph@gmail.com>
2026-01-28 12:33:11 +0100

    Secure XML parsing

--- surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/util/XmlUtils.java
+++ surefire-cached-extension/src/main/java/com/github/seregamorph/maven/test/util/XmlUtils.java
@@ -2,6 +2,7 @@ package com.github.seregamorph.maven.test.util;
 
 import java.io.File;
 import java.io.IOException;
+import javax.xml.XMLConstants;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
@@ -16,6 +17,19 @@ public final class XmlUtils {
     public static Document parseXml(File file) {
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+            factory.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+            // Disable DTDs entirely
+            factory.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
+            // Disable external entities
+            factory.setFeature("http://xml.org/sax/features/external-general-entities", false);
+            factory.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+            // Disable external DTDs
+            factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_DTD, "");
+            factory.setAttribute(XMLConstants.ACCESS_EXTERNAL_SCHEMA, "");
+
+            factory.setXIncludeAware(false);
+            factory.setExpandEntityReferences(false);
+
             DocumentBuilder docBuilder = factory.newDocumentBuilder();
             return docBuilder.parse(file);
         } catch (ParserConfigurationException | SAXException | IOException e) {
