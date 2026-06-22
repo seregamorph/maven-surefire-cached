d3170a7086d7b7380c55c252e7d1a78cdfe3870d
seregamorph <serega.morph@gmail.com>
2026-06-22 13:59:50 +0200

    Revert to Jackson 2

--- surefire-cached-common/src/main/java/com/github/seregamorph/maven/test/util/JsonSerializers.java
+++ surefire-cached-common/src/main/java/com/github/seregamorph/maven/test/util/JsonSerializers.java
@@ -1,39 +1,50 @@
 package com.github.seregamorph.maven.test.util;
 
-import tools.jackson.core.JacksonException;
-import tools.jackson.core.util.DefaultIndenter;
-import tools.jackson.core.util.DefaultPrettyPrinter;
-import tools.jackson.databind.SerializationFeature;
-import tools.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.PrettyPrinter;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.IOException;
+import java.io.UncheckedIOException;
 
 /**
  * @author Sergey Chernov
  */
 public final class JsonSerializers {
 
-    private static final JsonMapper mapper = JsonMapper.builder()
+    private static final ObjectMapper mapper = new ObjectMapper()
         .enable(SerializationFeature.INDENT_OUTPUT)
-        .defaultPrettyPrinter(createPrettyPrinter())
-        .findAndAddModules()
-        .build();
+        .setDefaultPrettyPrinter(createPrettyPrinter())
+        .registerModules(ObjectMapper.findModules(JsonSerializers.class.getClassLoader()))
+        .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
 
     public static byte[] serialize(Object obj) {
-        return mapper.writeValueAsBytes(obj);
+        try {
+            return mapper.writeValueAsBytes(obj);
+        } catch (JsonProcessingException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     public static String serializeAsString(Object obj) {
-        return mapper.writeValueAsString(obj);
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new UncheckedIOException(e);
+        }
     }
 
     public static <T> T deserialize(byte[] content, Class<T> type, String fileName) {
         try {
             return mapper.readValue(content, type);
-        } catch (JacksonException e) {
-            throw new RuntimeException("Failed to deserialize " + fileName, e);
+        } catch (IOException e) {
+            throw new UncheckedIOException("Failed to deserialize " + fileName + " " + e, e);
         }
     }
 
-    private static DefaultPrettyPrinter createPrettyPrinter() {
+    private static PrettyPrinter createPrettyPrinter() {
         DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
         DefaultPrettyPrinter.Indenter indenter = new DefaultIndenter("  ", DefaultIndenter.SYS_LF);
         prettyPrinter.indentArraysWith(indenter);
